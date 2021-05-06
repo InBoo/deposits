72df03e4634408ef04044960c8bb06ff1eeeee06
Inga <inga.bozoka@gmail.com>
2021-05-06 23:42:12 +0300

    Deposits quality update

--- deposits/urls.py
+++ deposits/urls.py
@@ -7,10 +7,8 @@ from . import views
 app_name = 'deposits'
 urlpatterns = [
     path('', views.DepositListView.as_view(), name='index'),
-    #path('get_user/<int:pk>', views.UserDetailView.as_view()),
-    path('deposit/new', views.AddDepositView.as_view()),
-    #path('edit_user/<int:pk>/', views.EditUserView.as_view()),
-    #path('delete_user/<int:pk>/', views.DeleteUserView.as_view()),
+    path('deposit/<int:pk>/', views.DepositDetailView.as_view(), name='get-deposit'),
+    path('deposit/new/', views.AddDepositView.as_view()),
 ]
 
 
--- deposits/views.py
+++ deposits/views.py
@@ -1,30 +1,72 @@
 
 from django.views.generic import FormView, ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from deposits.models import Deposit
+from deposits.models import Deposit as DepositDB
 from deposits.forms import DepositForm
+
+
+
+
+
+
+
+
 
 class Deposit():
     def __init__(self, deposit, term, rate):
-        self.deposit = deposit
-        self.term = term
-        self.rate = rate
-        self.interest = 0.0
 
+        # Sākotnējā noguldīguma summa
+        self.deposit = deposit
+
+        # Noguldījuma termiņš gados
+        self.term = term
+
+        # Gada procentu likme, decimālskaitlis, 5%=0.05
+        self.rate = rate
+
+
+    # Aprēķina peļņu noguldījuma termiņa beigās
     def interest(self):
+
+        total_interest = 0.0
         year = 0
         while year < self.term:
             interest = self.deposit * self.rate
-            self.interest += interest
+            total_interest += interest
             self.deposit += interest
             year += 1
 
+        return total_interest
             
             
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # https://docs.djangoproject.com/en/3.2/ref/class-based-views/generic-display/#listview
 class DepositListView(ListView):
-    model = Deposit
+    model = DepositDB
+
+
+
+
+
+
+class DepositDetailView(DetailView):
+    model = DepositDB
 
 
 
@@ -35,14 +77,33 @@ class DepositListView(ListView):
 class AddDepositView(FormView):
 
     form_class = DepositForm
-    template_name = 'deposit/form.html'
-    success_url = reverse_lazy('index')
+    template_name = 'deposits/form.html'
+    success_url = reverse_lazy('deposits:index')
 
     def form_valid(self, form):
-        form.save()
+        deposit_db = form.save(commit=False)
+        deposit = Deposit(
+            deposit=deposit_db.deposit,
+            term=deposit_db.term,
+            rate=deposit_db.rate,
+        )
 
-        response = super().form_valid(form)
-        return response
+        deposit_db.interest = deposit.interest()
+
+        deposit_db.save()
+
+
+        return super().form_valid(form)
+ 
+
+
+
+
+
+
+
+
+
 
 
 
